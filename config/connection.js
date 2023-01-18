84771287660ba7e0226d9ec58fed3d71917909ea
Inder <112728880+inderjz@users.noreply.github.com>
2023-01-17 19:07:28 -0800

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -9,7 +9,7 @@ let sequelize;
 if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
-  sequelize = new Sequelize(process.env.DB_NAME = 'tech_blog_db', process.env.DB_USER = 'root', process.env.DB_PW = 'Livnoor11', {
+  sequelize = new Sequelize(process.env.DB_NAME = '', process.env.DB_USER = '', process.env.DB_PW = '', {
     host: 'localhost',
     dialect: 'mysql',
     port: 3306
